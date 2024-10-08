0c45bad4505dbd2f9b581a38613d3df479000262
unknown <thaiwire>
2024-10-08 16:01:43 +0700

    signup

--- app/(loged-out)/sign-up/page.tsx
+++ app/(loged-out)/sign-up/page.tsx
@@ -18,24 +18,33 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { SelectContent } from "@radix-ui/react-select";
 import { PersonStandingIcon } from "lucide-react";
 import Link from "next/link";
+import { comma } from "postcss/lib/list";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
 const formSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(8),
+  accountType: z.enum(["personal", "company"]),
+  companyName: z.string().optional(),
+  numberOfEmployees: z.coerce.number().optional(),
 });
 
-export default function LoginPage() {
+export default function SignupPage() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
-      password: "",
     },
   });
 
@@ -48,9 +57,9 @@ export default function LoginPage() {
       <PersonStandingIcon size={50} />
       <Card className="w-full max-w-sm">
         <CardHeader>
-          <CardTitle>Login</CardTitle>
+          <CardTitle>Sign Up</CardTitle>
         </CardHeader>
-        <CardDescription>Login to Support TWP</CardDescription>
+        <CardDescription>Sign Up to New Account</CardDescription>
         <CardContent>
           <Form {...form}>
             <form
@@ -66,33 +75,41 @@ export default function LoginPage() {
                     <FormControl>
                       <Input placeholder="" {...field} type="email" />
                     </FormControl>
-                    <FormDescription>This is EMAIL.</FormDescription>
+
                     <FormMessage />
                   </FormItem>
                 )}
               />
               <FormField
                 control={form.control}
-                name="password"
+                name="accountType"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input placeholder="" {...field} type="password" />
-                    </FormControl>
-                    <FormDescription>This is Password</FormDescription>
+                    <FormLabel>Account Type</FormLabel>
+                    <Select onValueChange={field.onChange}>
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select Account Type" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="personal">Personal</SelectItem>
+                        <SelectItem value="company">Company</SelectItem>
+                      </SelectContent>
+                    </Select>
                     <FormMessage />
                   </FormItem>
                 )}
               />
-              <Button type="submit">Login</Button>
+
+              <Button type="submit">Sign Up</Button>
             </form>
           </Form>
         </CardContent>
         <CardFooter className="justify-between">
-          <small>Don't have account ?</small>
+          <small>Already account ?</small>
           <Button asChild variant="outline" size="sm">
-            <Link href="/sign-up">Sign Up</Link>
+            <Link href="/login">Login</Link>
           </Button>
         </CardFooter>
       </Card>
